ea0e6d9ac2a88ff4f399b1ac4d12d41e8ab127b3
lucassdev01 <luh.raimundoalves@gmail.com>
2026-01-08 17:13:40 -0300

    Btn add carrinho
    Botão de adicionar ao carrinho no cardapio.

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ function abrirModal() {
 function fecharModal() {
   document.getElementById('modal').style.display = 'none';
 }
+
+function adicionarCarrinho(nome, preco) {
+  let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+
+  const item = carrinho.find(p => p.nome === nome);
+
+  if (item) {
+    item.qtd++;
+  } else {
+    carrinho.push({ nome, preco, qtd: 1 });
+  }
+
+  localStorage.setItem("carrinho", JSON.stringify(carrinho));
+
+  mostrarMensagem();
+}
+
+function mostrarMensagem() {
+  const toast = document.getElementById("toast");
+  toast.classList.add("show");
+
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 2500);
+}
